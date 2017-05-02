c742aa8ecfba6aed2666663eb14e6c22699bcd57
Joseph Lambert <Lambert@Josephs-MacBook-Pro-2.local>
2017-05-01 20:17:26 -0700

    earth and stuff

--- src/Containers/ApplicationContainer.js
+++ src/Containers/ApplicationContainer.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import RSSFeedContainer from './RSSFeedContainer';
+
+const rotate = keyframes`
+  from {
+    transform: rotateY(0deg);
+  }
+
+  to {
+    transform: rotateY(359deg);
+  }
+`;
+
+const AppContainer = styled.div`
+  padding-top: 200px;
+`;
+
+const TitleContainer = styled.div`
+  position: fixed;
+    top: 25px;
+    left: 20px;
+    right: 20px;
+    z-index: 20;
+`;
 
 const PodcastTitle = styled.h1`
   display: flex;
     justify-content: center;
     align-items: center;
-  margin: 50px 0;
-  transform: skew(-6deg);
+
+  font-family: Times New Roman;
+  font-weight: normal;
+  font-size: 10rem;
+  text-decoration: underline;
+`;
+
+const EarthImage = styled.span`
+  background-image: url('./Assets/images/flat-earth.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  margin: 0 10px;
+  transform: rotateY(0deg);
+  animation: ${rotate} 5s linear infinite;
 `;
 
 class ApplicationContainer extends Component {
   render() {
     return (
-      <div>
-        <PodcastTitle>Trust</PodcastTitle>
-        <PodcastTitle>Issues</PodcastTitle>
+      <AppContainer>
+        <TitleContainer>
+          <PodcastTitle>Trust<EarthImage />Issues</PodcastTitle>
+        </TitleContainer>
         <RSSFeedContainer />
-      </div>
+        </AppContainer>
     );
   }
 }
